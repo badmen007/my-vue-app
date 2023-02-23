4b0d47722001b5b967a99a84fb60bf0285c636f1
xz <1572226525@qq.com>
2023-02-23 21:46:14 +0800

    ''

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  singleQuote: true, // 单引号
+  semi: false, // 不加分号
+  trailingComma: 'none', // 不加尾逗号
+  tabWidth: 2, // tab宽度
+  useTabs: false, // 不使用tab
+  endOfLine: 'auto', // 换行符
+}

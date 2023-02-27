c7e2bff48820ea14b5be8713a3ff94464563a15c
xz <1572226525@qq.com>
2023-02-27 21:14:59 +0800

    feat: sidebar 组件基本样式及配置

--- src/components.d.ts
+++ src/components.d.ts
@@ -11,8 +11,14 @@ declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     Counter: typeof import('./components/Counter/index.vue')['default']
     ElButton: typeof import('element-plus/es')['ElButton']
+    ElIcon: typeof import('element-plus/es')['ElIcon']
+    ElMenu: typeof import('element-plus/es')['ElMenu']
+    ElMenuItem: typeof import('element-plus/es')['ElMenuItem']
+    ElSlider: typeof import('element-plus/es')['ElSlider']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
+    Sidebar: typeof import('./layout/components/Sidebar/index.vue')['default']
+    SidebarItem: typeof import('./layout/components/Sidebar/SidebarItem.vue')['default']
     SvgIcon: typeof import('./components/SvgIcon/index.vue')['default']
   }
 }
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,77 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Layout from '@/layout/index.vue'
 
-const routes: Array<RouteRecordRaw> = [
+export const asyncRoutes: Array<RouteRecordRaw> = [
+  {
+    path: '/documentation',
+    component: Layout,
+    redirect: '/documentation/index',
+    children: [
+      {
+        path: 'index',
+        name: 'Documentation',
+        component: () => import('@/views/documentation/index.vue'),
+        meta: {
+          title: 'Documentation',
+          icon: 'documentation'
+        }
+      }
+    ]
+  },
+  {
+    path: '/guide',
+    component: Layout,
+    redirect: '/guide/index',
+    children: [
+      {
+        path: 'index',
+        name: 'Guide',
+        component: () => import('@/views/guide/index.vue'),
+        meta: {
+          title: 'Guide',
+          icon: 'guide'
+        }
+      }
+    ]
+  },
+  {
+    path: '/system',
+    component: Layout,
+    redirect: '/system/user',
+    meta: {
+      title: 'System',
+      icon: 'lock'
+    },
+    children: [
+      {
+        path: 'menu',
+        component: () => import('@/views/system/menu.vue'),
+        meta: {
+          title: 'Menu Management',
+          icon: 'list'
+        }
+      },
+      {
+        path: 'role',
+        component: () => import('@/views/system/role.vue'),
+        meta: {
+          title: 'Role Management',
+          icon: 'list'
+        }
+      },
+      {
+        path: 'user',
+        component: () => import('@/views/system/user.vue'),
+        meta: {
+          title: 'User Management',
+          icon: 'list'
+        }
+      }
+    ]
+  }
+]
+
+const constantRoutes: Array<RouteRecordRaw> = [
   {
     path: '/',
     component: Layout,
@@ -16,6 +86,8 @@ const routes: Array<RouteRecordRaw> = [
     ]
   }
 ]
+
+export const routes = [...constantRoutes, ...asyncRoutes]
 
 export default createRouter({
   history: createWebHistory(),

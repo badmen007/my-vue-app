bb8b6c227a12e895a7b85bef2714631b82a501dc
xz <1572226525@qq.com>
2023-03-09 22:08:34 +0800

    feat: 添加 logo 并控制显示隐藏

--- src/layout/components/Sidebar/index.vue
+++ src/layout/components/Sidebar/index.vue
@@ -1,29 +1,31 @@
 <template>
-  <Logo v-if="showLogo" :collapse="isCollapse"/>
-  <el-menu
-    class="sidebar-container-menu"
-    mode="vertical"
-    :default-active="activeMenu"
-    :background-color="scssVariables.menuBg"
-    :text-color="scssVariables.menuText"
-    :collapse="sidebar.opened"
-    :collapse-transition="true"
-    :active-text-color="themeColor"
-  >
-    <sidebar-item
-      v-for="route in menuRoutes"
-      :key="route.path"
-      :item="route"
-      :base-path="route.path"
-    />
-  </el-menu>
+  <Logo v-if="showLogo" :collapse="isCollapse" />
+  <scroll-panel>
+    <el-menu
+      class="sidebar-container-menu"
+      mode="vertical"
+      :default-active="activeMenu"
+      :background-color="scssVariables.menuBg"
+      :text-color="scssVariables.menuText"
+      :collapse="sidebar.opened"
+      :collapse-transition="true"
+      :active-text-color="themeColor"
+    >
+      <sidebar-item
+        v-for="route in menuRoutes"
+        :key="route.path"
+        :item="route"
+        :base-path="route.path"
+      />
+    </el-menu>
+  </scroll-panel>
 </template>
 <script lang="ts" setup>
 import scssVariables from '@/styles/variables.module.scss'
 import { routes } from '@/router'
-import { useAppStore } from "@/stores/app";
-import { storeToRefs } from "pinia";
-import { useSettingStore } from '@/stores/settings';
+import { useAppStore } from '@/stores/app'
+import { storeToRefs } from 'pinia'
+import { useSettingStore } from '@/stores/settings'
 
 const store = useAppStore()
 const { sidebar } = storeToRefs(store)
@@ -47,4 +49,14 @@ const themeColor = computed(() => settingStore.settings.theme)
 const showLogo = computed(() => settingStore.settings.sidebarLogo)
 // 展开收起状态 稍后放store 当前是展开就让他收起
 const isCollapse = computed(() => sidebar.value.opened)
+
+const logoHeight = computed(() => (showLogo.value ? '50' : 0) + 'px')
 </script>
+
+<style lang="scss" scoped>
+.sidebar-wrapper {
+  .sidebar-container-menu {
+    height: calc(100vh - v-bind(logoHeight));
+  }
+}
+</style>
